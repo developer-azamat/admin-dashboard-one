aaad3179c376d34c3b330ba0b3365da5e6258d6f
Amirxon <shokirovamir2006@gmail.com>
2023-06-22 00:48:24 +0500

    Sidebar updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import "./App.css"
 import Home from './components/pages/Sidebar/Home'
+import Main from './components/pages/main-info/Main'
 
 const App = () => {
   return (
     <div className='App flex'>
       <Home/>
+      <Main/>
     </div>
   )
 }
