dc8ec3a6015e5f09378d575a90520a51d9fae852
azamat-developer <azamatjonazimov561@gmail.com>
2023-06-22 12:50:52 +0500

    new update

--- src/components/pages/workers/Workers.jsx
+++ src/components/pages/workers/Workers.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import DenseTable from '../../DenseTable'
+import Example from "../../Example"
 function Workers() {
 	return (
-		<div>Worker</div>
+		<div className='px-10 py-5'>
+			<div className='flex items-center gap-5 bg-white/20 '>
+				<Example />
+				<Example />
+				<Example />
+			</div>
+			<DenseTable /></div>
 	)
 }
 
