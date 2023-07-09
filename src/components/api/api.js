f28a51695f449bf6623a5aeae2648258747840bf
azamat-developer <azamatjonazimov561@gmail.com>
2023-07-09 22:14:32 +0500

    cmm

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import { getItem } from '../../helpers/persistence-log'
 
 const api_url = 'https://moneymanager.pythonanywhere.com'
 const api = axios.create({
@@ -8,8 +7,9 @@ const api = axios.create({
 api.interceptors.request.use(config => {
 	const token =
 		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjg5MDk2OTMwLCJpYXQiOjE2ODg4Mzc3MzAsImp0aSI6ImZmMjVhZTUxZjBlNDQ5YzI4NzgyNzgyNWM3MDUzYTI1IiwidXNlcl9pZCI6MX0.ztTS_Yi72yhYDiOH3uhByxhMQd_Y83Mx3e6IT_dX8uM'
-	const authorization = token ? `Bearer ${token}` : ""
+	const authorization = token ? `Bearer ${token}` : ''
 	config.headers['Authorization'] = authorization
+	config.headers['Content-Type'] = 'application/json'
 	return config
 })
 export default api
